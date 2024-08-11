8ee4637dfdbe8170279eacb1703f9d3fc598c0ef
Ankit <aank2it@gmail.com>
2024-08-11 17:43:01 +0530

    first commit

---
+++ Basics.js
@@ -0,0 +1,50 @@
+//-------BASIC PRACTICE QUESTIONS---------
+
+
+// "" + 1 + 0
+console.log("" + 1 + 0)
+
+// "" - 1 + 0
+console.log("" - 1 + 0);
+
+// true + false
+console.log(true + false);
+
+// 6 / "3"
+console.log(6 / "3");
+
+// "2" * "3"
+console.log("2" * "3");
+
+// 4 + 5 + "px"
+console.log(4 + 5 + "px")
+
+// "$" + 4 + 5
+console.log("$" + 4 + 5);
+
+// "4" - 2
+console.log("4" - 2);
+
+// "4px" - 2
+console.log("4px - 2");
+
+// 7 / 0
+console.log(7 / 0);
+
+// " -9 " + 5
+console.log(" -9 " + 5);
+
+// " -9 " - 5
+console.log( " -9 " -5);
+
+// null + 1
+console.log(null + 1);
+
+
+let ab = 2;
+let xq = 1 + (ab *= 2);
+console.log(xq);
+
+
+// undefined + 1
+console.log(undefine + 1);
