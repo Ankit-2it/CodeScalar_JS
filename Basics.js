b22ce1f57e348cbde1b2ac88442144fa9a0e4cdb
Ankit <aank2it@gmail.com>
2024-08-16 09:47:18 +0530

    first commit

--- Basics.js
+++ Basics.js
@@ -1,50 +1,55 @@
 //-------BASIC PRACTICE QUESTIONS---------
 
 
-// "" + 1 + 0
-console.log("" + 1 + 0)
+// // "" + 1 + 0
+// console.log("" + 1 + 0)
 
-// "" - 1 + 0
-console.log("" - 1 + 0);
+// // "" - 1 + 0
+// console.log("" - 1 + 0);
 
-// true + false
-console.log(true + false);
+// // true + false
+// console.log(true + false);
 
-// 6 / "3"
-console.log(6 / "3");
+// // 6 / "3"
+// console.log(6 / "3");
 
-// "2" * "3"
-console.log("2" * "3");
+// // "2" * "3"
+// console.log("2" * "3");
 
-// 4 + 5 + "px"
-console.log(4 + 5 + "px")
+// // 4 + 5 + "px"
+// console.log(4 + 5 + "px")
 
-// "$" + 4 + 5
-console.log("$" + 4 + 5);
+// // "$" + 4 + 5
+// console.log("$" + 4 + 5);
 
-// "4" - 2
-console.log("4" - 2);
+// // "4" - 2
+// console.log("4" - 2);
 
-// "4px" - 2
-console.log("4px - 2");
+// // "4px" - 2
+// console.log("4px - 2");
 
-// 7 / 0
-console.log(7 / 0);
+// // 7 / 0
+// console.log(7 / 0);
 
-// " -9 " + 5
-console.log(" -9 " + 5);
+// // " -9 " + 5
+// console.log(" -9 " + 5);
 
-// " -9 " - 5
-console.log( " -9 " -5);
+// // " -9 " - 5
+// console.log( " -9 " -5);
 
-// null + 1
-console.log(null + 1);
-
-
-let ab = 2;
-let xq = 1 + (ab *= 2);
-console.log(xq);
+// // null + 1
+// console.log(null + 1);
 
 
-// undefined + 1
-console.log(undefine + 1);
+// let ab = 2;
+// let xq = 1 + (ab *= 2);
+// console.log(xq);
+
+// let w = 3
+// console.log(~w)
+
+console.log(5|10)
+console.log(2||3)
+
+// // undefined + 1
+// console.log(undefine + 1);
